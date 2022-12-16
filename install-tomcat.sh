439d6c75f348f396dbe3e2c25e3fb025e8d3d3bd
svc-devops-at-191323423716 <info@cloudbinary.io>
2022-12-16 11:18:10 +0530

    pipeline code

---
+++ install-tomcat.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+hostnamectl set-hostname "tomcat.cloudbinary.io"
+echo "`hostname -I | awk '{ print $1}'` `hostname`" >> /etc/hosts
+sudo apt-get update
+sudo apt-get install git curl wget unzip tree -y 
+sudo apt-get install software-properties-common -y 
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y 
+
+cd /opt/
+git clone https://github.com/kesavkummari/tomcat-role.git
+
+cd tomcat-role
+
+ls -lrt tomcat-setup.yml
+
+ls -ld tomcat
+
+ansible-playbook tomcat-setup.yml
+
+# cd /usr/share/tomcat/webapps/
+
+# # wget https://github.com/kesavkummari/opscloud/blob/main/target/opscloud-5.0.0.war 
+
+# chmod 777 ROOT.war 
+# chown -R tomcat:tomcat ROOT.war
+
+# /usr/share/tomcat/webapps/
